bf8f3fbdda3b5e2e2eb75b0e3c1454ca4e7d916c
jalsin-nln <jalal.singleton@nextlevelnola.org>
2023-02-07 20:21:05 +0000

    completed walker projected

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,14 +10,24 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+  var KEY = {
+    "ENTER": 13,
+    "UP":38,
+    "DOWN":40,
+    "LEFT":37,
+    "RIGHT":39
+  };
   // Game Item Objects
 
 
   // one-time setup
+  var positionx=0;
+  var positiony=0;
+  var speedx=0;
+  var speedy=0;
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
-
+  $(document).on('keyup', handleKeyUp);
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -27,7 +37,8 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
+    repositionGameItem();
+    redrawGameItem();
 
   }
   
@@ -35,7 +46,22 @@ function runProgram(){
   Called in response to events.
   */
   function handleKeyDown(event) {
-    console.log(39);
+    if (event.which === KEY.LEFT){
+      speedx = -5;
+    }
+
+    else if (event.which === KEY.RIGHT){
+      speedx = 5;
+     
+    }
+    else if (event.which === KEY.UP){
+      speedy = -5;
+      
+    }
+    else if (event.which === KEY.DOWN){
+      speedy = 5;
+      
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -50,5 +76,17 @@ function runProgram(){
     // turn off event handlers
     $(document).off();
   }
+ function repositionGameItem(){
+   positionx += speedx;
+   positiony += speedy;
+  }
+  function redrawGameItem(){
+    $("#walker").css("top", positiony);
+    $("#walker").css("left", positionx);
   
+  }
+  function handleKeyUp(){
+    speedx =0;
+    speedy =0;
+  }
 }
