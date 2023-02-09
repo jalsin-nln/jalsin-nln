34546a7b5fd04a35de7e2d90e49594a222430a24
jalsin-nln <jalal.singleton@nextlevelnola.org>
2023-02-09 23:20:22 +0000

    completed challenges

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -11,23 +11,36 @@ function runProgram(){
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   var KEY = {
-    "ENTER": 13,
     "UP":38,
     "DOWN":40,
     "LEFT":37,
-    "RIGHT":39
+    "RIGHT":39,
+    "W":87,
+    "A":65,
+    "S":83,
+    "D":68,
   };
+  var BOARD_WIDTH = $("#board").width() - $("#walker").width();
+  var BOARD_HEIGHT = $("#board").height() - $("#walker").height();
+  var LEFT_SIDE = 0;
+  var TOP_SIDE = 0;
+  
   // Game Item Objects
 
 
   // one-time setup
-  var positionx=0;
-  var positiony=0;
-  var speedx=0;
-  var speedy=0;
+  var positionX=0;
+  var positionY=0;
+  var speedX=0;
+  var speedY=0;
+  var secondPositionX=390;
+  var secondPositionY=390;
+  var secondSpeedX=0;
+  var secondSpeedY=0;
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
   $(document).on('keyup', handleKeyUp);
+
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -38,8 +51,11 @@ function runProgram(){
   */
   function newFrame() {
     repositionGameItem();
+    repositionPlayerTwo();
+    setBoundary();
     redrawGameItem();
-
+    redrawPlayerTwo();
+    
   }
   
   /* 
@@ -47,20 +63,29 @@ function runProgram(){
   */
   function handleKeyDown(event) {
     if (event.which === KEY.LEFT){
-      speedx = -5;
+      speedX = -5;
     }
 
     else if (event.which === KEY.RIGHT){
-      speedx = 5;
-     
+      speedX = 5;
     }
     else if (event.which === KEY.UP){
-      speedy = -5;
-      
+      speedY = -5;
     }
     else if (event.which === KEY.DOWN){
-      speedy = 5;
-      
+      speedY = 5;    
+    }
+    else if (event.which === KEY.W){
+      secondSpeedY = -5;
+    }
+    else if (event.which === KEY.A){
+      secondSpeedX = -5;
+    }
+    else if (event.which === KEY.S){
+      secondSpeedY = 5;
+    }
+    else if (event.which === KEY.D){
+      secondSpeedX = 5;
     }
   }
 
@@ -77,16 +102,55 @@ function runProgram(){
     $(document).off();
   }
  function repositionGameItem(){
-   positionx += speedx;
-   positiony += speedy;
+   positionX += speedX;
+   positionY += speedY;
   }
+ function repositionPlayerTwo(){
+  secondPositionX += secondSpeedX;
+  secondPositionY += secondSpeedY;
+ }
   function redrawGameItem(){
-    $("#walker").css("top", positiony);
-    $("#walker").css("left", positionx);
+    $("#walker").css("top", positionY);
+    $("#walker").css("left", positionX);
   
   }
-  function handleKeyUp(){
-    speedx =0;
-    speedy =0;
+  function redrawPlayerTwo(){
+    $("#playerTwo").css("top",secondPositionY);
+    $("#playerTwo").css("left",secondPositionX);
   }
+  function handleKeyUp(){
+    speedX =0;
+    speedY =0;
+    secondSpeedX=0;
+    secondSpeedY=0;
+  }
+ function setBoundary(){
+  //checking the position of the walker
+  if (positionX >= BOARD_WIDTH){
+   positionX = BOARD_WIDTH;
+  }
+  if ( positionX <= LEFT_SIDE){
+    positionX =  LEFT_SIDE;
+  }
+  if (positionY >= BOARD_HEIGHT){
+  positionY = BOARD_HEIGHT;
+ }
+  if (positionY <= TOP_SIDE ){
+  positionY = TOP_SIDE;
+ }//checking the position of player two
+ if (secondPositionX >= BOARD_WIDTH){
+  secondPositionX = BOARD_WIDTH;
+ }
+ if (secondPositionX <= LEFT_SIDE){
+  secondPositionX = LEFT_SIDE;
+ }
+ if(secondPositionY >= BOARD_HEIGHT){
+  secondPositionY = BOARD_HEIGHT;
+ }
+ if(secondPositionY <= TOP_SIDE){
+  secondPositionY = TOP_SIDE;
+ }
+ }
+ 
+
 }
