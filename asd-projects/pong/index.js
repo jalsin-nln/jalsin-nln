40fd6c0b39bb97e2799defa5e5a29c891ce80cce
jalsin-nln <jalal.singleton@nextlevelnola.org>
2023-04-11 20:52:24 +0000

    saving pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -10,14 +10,34 @@ function runProgram(){
   // Constant Variables
   const FRAME_RATE = 60;
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+  const UP_ARROW_KEY = 38;
+  const DOWN_ARROW_KEY = 40;
+  const W_KEY = 87;
+  const S_KEY = 83;
   // Game Item Objects
-
+  function GamePiece(id) {
+    var GamePieceInstance = {}
+    GamePieceInstance.id = id;
+    GamePieceInstance.x = $(id).css("left");
+    GamePieceInstance.y = $(id).css("top");
+    GamePieceInstance.width = $(id).width();
+    GamePieceInstance.height = $(id).height();
+    GamePieceInstance.speedX = 0;
+    GamePieceInstance.speedY = 0;
+    return GamePieceInstance;
+  }
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
-
+  var positionX =0;
+  var positionY =0;
+  var speedX = 0;
+  var speedY = 0;
+  var secondSpeedX = 0;
+  var secondSpeedY = 0;
+  var secondPositionX = $("#playerTwo").css("left");
+  var secondPositionY = $("#playerTwo").css("top");
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -35,7 +55,18 @@ function runProgram(){
   Called in response to events.
   */
   function handleEvent(event) {
-
+    if (event.which === W_KEY) {
+      speedY = 5;
+    }
+    if (event.which === S_KEY) {
+      speedY = -5;
+    }
+    if (event.which === UP_ARROW_KEY) {
+      secondSpeedY = 5;
+    }
+    if (event.which === DOWN_ARROW_KEY) {
+      secondSpeedY = -5;
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
